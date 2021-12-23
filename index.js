d1dde02dc5f1cd4854a36176a35aaf983ec55b33
adilanakbar11 <91605602+adilanakbar11@users.noreply.github.com>
2021-12-23 23:34:06 +0700

    Update index.js

--- index.js
+++ index.js
@@ -194,8 +194,8 @@ autoketik = false
 autoregister = setting.user_register
 
 img = setting.img
-baper = 'SUB YT ZEEONE OFC'
-apiku = 'https://youtu.be/2rWbjrDIAyA'
+baper = 'SUBS YT LAXY GAMING 🎮'
+apiku = 'https://youtube.com/channel/UCOnG-upCYgeUM5pKoK22ezA'
 gc_wa_lu = 'https://chat.whatsapp.com/EU890BcXjyBDkNaUT5WmYV' //klo gk punya gc wa gk usah di ganti 👍
 targetpc = setting.ownerNumberr
 owner = targetpc
